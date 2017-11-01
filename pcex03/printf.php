f99091cbcc64cb61eedc2335b28a3f19a58b2d16
Vladimir Maric <vladimirmaric@icloud.com>
2017-11-01 11:19:12 +0100

    ...

---
+++ pcex03/printf.php
@@ -0,0 +1,17 @@
+<?php
+printf("Price for this shirt is %.2f euro",0.5);
+
+print(PHP_EOL);
+
+printf("Number %d is %b in binary",8,8);
+
+print(PHP_EOL);
+
+printf("Number %d is %b in binary, and %X in hexadecimal",12,12,12);
+
+print(PHP_EOL);
+
+printf("Match winner odd for this match is %.1f",12.30);
+
+print(PHP_EOL);
+
